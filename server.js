725cfc6d90665805ff147067b0605dc82995d9c0
akshat-315 <akshat315.sh@gmail.com>
2024-04-20 10:21:18 +0530

    Speeded up the server using ioredis

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const axios = require("axios");
+const client = require("./client");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const cache = await client.get("todos");
+
+  if (cache) return res.json(JSON.parse(cache));
+
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/todos"
+  );
+
+  await client.set("todos", JSON.stringify(data));
+  await client.expire("todos", 20);
+  return res.json(data);
+});
+
+app.listen(9000);
